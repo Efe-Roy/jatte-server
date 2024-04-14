4a484c21c3ef59dd728249f6fe95d26b53c8a2de
Efe-Roy <dakaraefe@gmail.com>
2024-04-14 17:52:11 +0100

    new test

--- jatte/urls.py
+++ jatte/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('', include('chat.urls')),
     path('', include('account.urls')),
     path('', include('store.urls')),
-    path('admin/', admin.site.urls),
 ]
