73c64f98a28c33c4be45590a5c0c71124d050b15
Efe-Roy <dakaraefe@gmail.com>
2024-04-14 17:17:30 +0100

    new change

--- jatte/settingsprod.py
+++ jatte/settingsprod.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%v$gh67imza=0$i%pky!jxpk*@%t+x-w$lw5lmwbvj)+#p=r#g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['jatte.fiscaliaycontraloria.com', '10.0.2.2', '143.198.143.239']
+ALLOWED_HOSTS = ['jatte.fiscaliaycontraloria.com', '10.0.2.2', '143.198.143.239', 'subdomain.fiscaliaycontraloria.com']
 
 SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 
